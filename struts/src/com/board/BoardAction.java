39b39b2ce5dda303b837459fd2d0f8fc98beccf2
ruruj <ruruj@LAPTOP-99A92QO4>
2024-12-14 23:39:09 +0900

    build path

--- struts/src/com/board/BoardAction.java
+++ struts/src/com/board/BoardAction.java
@@ -9,8 +9,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
